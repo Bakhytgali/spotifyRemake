5bcc9e31cb6edb1247adf85086d11b9baeeacd02
Bakhytgali <omgwtfwat@gmail.com>
2024-02-05 00:35:20 +0600

    Back-End Changes. Getting user information using access token

--- routes/account.js
+++ routes/account.js
@@ -1,20 +1,50 @@
 const express = require("express");
 const SpotifyWebApi = require("spotify-web-api-node");
+const {authorizationCodeGrant} = require("spotify-web-api-node/src/server-methods");
 
-const clientId = 'b1c7aad75bd7477cbba728e68d4bc772';
-const clientSecret = 'e6128e223e5a42cb991d108f6e5f6990';
-const redirectUri = 'http://localhost:5000/account';
+let code;
 
-const spotifyApi = new SpotifyWebApi({
-    clientId: clientId,
-    clientSecret: clientSecret,
-    redirectUri: redirectUri
-});
+let email, username;
 
 const accountRouter = express.Router();
 
 accountRouter.get("/", async (req, res) => {
-        res.render("account");
+        code = req.query.code;
+
+        getUserInfo();
+
+        res.render("account", { username, email });
 });
+
+let spotify;
+
+async function getUserInfo() {
+    spotify = new SpotifyWebApi({
+        clientId: "b1c7aad75bd7477cbba728e68d4bc772",
+        clientSecret: "e6128e223e5a42cb991d108f6e5f6990",
+        redirectUri: "http://localhost:5000/account"
+    });
+
+    spotify.
+    authorizationCodeGrant(code)
+        .then(function(data) {
+
+            console.log(`Access token: ${data.body["access_token"]}`);
+
+            spotify.setAccessToken(data.body["access_token"]);
+
+            return spotify.getMe();
+        })
+        .then(function(data) {
+
+            email = data.body.email;
+            username = data.body["display_name"];
+
+        }).catch(
+        function(e) {
+            console.log(`Error! ${e.message}`);
+        }
+    )
+}
 
 module.exports = accountRouter;
