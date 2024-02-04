34b5d7ad50e68217bc118f3423bc530956f441d4
Bakhytgali <omgwtfwat@gmail.com>
2024-02-04 22:49:51 +0600

    Front-End changes / Deleting old Spotify Api usage

--- server.js
+++ server.js
@@ -9,11 +9,35 @@ app.use(express.static(__dirname));
 app.use("/styles", express.static(join(__dirname, "styles")));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { powerCards });
 });
 
 const accountRouter = require("./routes/account");
 
 app.use("/account", accountRouter);
+
+
+const powerCards = [
+    adFree = {
+        image: "/imgs/Image.svg",
+        title: "Ad-free music listening",
+        subtitle: "Enjoy uninterrupted music"
+    },
+    offline = {
+        image: "/imgs/Image-1.svg",
+        title: "Offline playback",
+        subtitle: "Save your data by listening offline"
+    },
+    everywhere = {
+        image: "/imgs/Image-2.svg",
+        title: "Play everywhere",
+        subtitle: "Listen on your speakers, TV, and other favorite devices"
+    },
+    payment = {
+        image: "/imgs/Image-3.svg",
+        title: "Pay your way",
+        subtitle: "Prepay with Paytm, UPI and more"
+    }
+];
 
 app.listen(port);
