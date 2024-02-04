a754ddb852a4112e20a6f38bfa9060420e66397b
Bakhytgali <omgwtfwat@gmail.com>
2024-02-05 01:37:38 +0600

    Back-End Changes. Better getUserInfo() function

--- routes/account.js
+++ routes/account.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const SpotifyWebApi = require("spotify-web-api-node");
-const {authorizationCodeGrant} = require("spotify-web-api-node/src/server-methods");
-
-let code;
-
-let email, username;
 
 const accountRouter = express.Router();
 
+let code;
+let email, username;
+
 accountRouter.get("/", async (req, res) => {
-        code = req.query.code;
+    code = req.query.code;
 
-        getUserInfo();
+    await getUserInfo();
 
-        res.render("account", { username, email });
+    res.render("account", { username, email });
 });
 
 let spotify;
@@ -25,26 +23,18 @@ async function getUserInfo() {
         redirectUri: "http://localhost:5000/account"
     });
 
-    spotify.
-    authorizationCodeGrant(code)
-        .then(function(data) {
+    try {
+        const data = await spotify.authorizationCodeGrant(code);
+        console.log(`Access token: ${data.body["access_token"]}`);
 
-            console.log(`Access token: ${data.body["access_token"]}`);
+        spotify.setAccessToken(data.body["access_token"]);
 
-            spotify.setAccessToken(data.body["access_token"]);
-
-            return spotify.getMe();
-        })
-        .then(function(data) {
-
-            email = data.body.email;
-            username = data.body["display_name"];
-
-        }).catch(
-        function(e) {
-            console.log(`Error! ${e.message}`);
-        }
-    )
+        const userData = await spotify.getMe();
+        email = userData.body.email;
+        username = userData.body.display_name;
+    } catch (e) {
+        console.log(`Error! ${e.message}`);
+    }
 }
 
 module.exports = accountRouter;
